822cd1914f2ff8685e0e067b25d64c98974e5abc
Facundo Personal <facundopellicer4@gmail.com>
2025-07-17 20:16:54 -0300

    fix import

--- apps/web/src/app/profile/[id]/following/page.tsx
+++ apps/web/src/app/profile/[id]/following/page.tsx
@@ -1,91 +1,89 @@
-import Image from "next/image";
-import { FollowService } from "@/follows/follow.service";
-import { FollowUserButton } from "@/users/components/follow-user-button";
-import { redirect } from "next/navigation";
-import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { FollowService } from "@/follows/follow.service";
+import { auth } from "@/lib/auth";
+import { FollowUserButton } from "@/users/components/follow-user-button";
 
 const fetchFollowingUsers = async (userId: string) => {
-  const followService = new FollowService();
-  const followingUsers = await followService.getFollowingUsers(userId);
-  return followingUsers;
+	const followService = new FollowService();
+	const followingUsers = await followService.getFollowingUsers(userId);
+	return followingUsers;
 };
 
 function showFollowButton(
-  sessionUserId: string,
-  profileUserId: string,
-  followeeId: string
+	sessionUserId: string,
+	profileUserId: string,
+	followeeId: string,
 ) {
-  const isSessionUserProfile = sessionUserId === profileUserId;
-  const isSameUser = sessionUserId === followeeId;
+	const isSessionUserProfile = sessionUserId === profileUserId;
+	const isSameUser = sessionUserId === followeeId;
 
-  return !isSessionUserProfile && !isSameUser;
+	return !isSessionUserProfile && !isSameUser;
 }
 
 export default async function FollowingPage(
-  props: Readonly<{
-    params: Promise<{ id: string }>;
-  }>
+	props: Readonly<{
+		params: Promise<{ id: string }>;
+	}>,
 ) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
 
-  if (!session) {
-    redirect("/");
-  }
+	if (!session) {
+		redirect("/");
+	}
 
-  const params = await props.params;
-  const profileUserId = params.id;
+	const params = await props.params;
+	const profileUserId = params.id;
 
-  const followingUsers = await fetchFollowingUsers(profileUserId);
+	const followingUsers = await fetchFollowingUsers(profileUserId);
 
-  if (followingUsers.length === 0) {
-    return (
-      <div className="flex-1 pt-10 text-neutral-500">No sigue a nadie</div>
-    );
-  }
+	if (followingUsers.length === 0) {
+		return (
+			<div className="flex-1 pt-10 text-neutral-500">No sigue a nadie</div>
+		);
+	}
 
-  const isFollowing = false;
+	const isFollowing = false;
 
-  return (
-    <section className="flex-1 pt-10 space-y-4">
-      {followingUsers.map((user) => (
-        <a
-          href={`/profile/${user.followeeId}`}
-          className="flex justify-between"
-          key={user.followeeId}
-        >
-          <div className="flex gap-4">
-            <div className="size-[50px] rounded-full bg-secondary-foreground">
-              <Image
-                unoptimized
-                src={user.userImage}
-                width={50}
-                height={50}
-                className="h-auto object-cover rounded-full"
-                alt={user.userName}
-              />
-            </div>
-            <div>
-              <div>{user.userName}</div>
-              <div>{user.userUsername}</div>
-            </div>
-          </div>
-          {showFollowButton(
-            session.user.id,
-            profileUserId,
-            user.followeeId
-          ) && (
-            <FollowUserButton
-              followedUserId={user.followeeId}
-              isFollowing={isFollowing}
-            >
-              {isFollowing ? "Siguiendo" : "Seguir"}
-            </FollowUserButton>
-          )}
-        </a>
-      ))}
-    </section>
-  );
+	return (
+		<section className="flex-1 pt-10 space-y-4">
+			{followingUsers.map((user) => (
+				<a
+					href={`/profile/${user.followeeId}`}
+					className="flex justify-between"
+					key={user.followeeId}
+				>
+					<div className="flex gap-4">
+						<div className="size-[50px] rounded-full bg-secondary-foreground">
+							<Image
+								unoptimized
+								src={user.userImage}
+								width={50}
+								height={50}
+								className="h-auto object-cover rounded-full"
+								alt={user.userName}
+							/>
+						</div>
+						<div>
+							<div>{user.userName}</div>
+							<div>{user.userUsername}</div>
+						</div>
+					</div>
+					{showFollowButton(
+						session.user.id,
+						profileUserId,
+						user.followeeId,
+					) && (
+						<FollowUserButton
+							followedUserId={user.followeeId}
+							isFollowing={isFollowing}
+						/>
+					)}
+				</a>
+			))}
+		</section>
+	);
 }
