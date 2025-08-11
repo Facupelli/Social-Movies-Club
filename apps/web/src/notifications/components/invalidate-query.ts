d5b3cd0b2628f61d5161ee44a97fcd4b252ec5fe
Facundo Personal <facundopellicer4@gmail.com>
2025-08-10 21:35:12 -0300

    fix notification query invalidation

--- apps/web/src/notifications/components/invalidate-query.ts
+++ apps/web/src/notifications/components/invalidate-query.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { QUERY_KEYS } from "@/lib/app.constants";
 import { markNotiAsRead } from "../actions/mark-noti-as-read";
@@ -8,17 +8,23 @@ import { markNotiAsRead } from "../actions/mark-noti-as-read";
 export function InvalidateNotificationsQuery() {
 	const queryClient = useQueryClient();
 
-	useEffect(() => {
-		markNotiAsRead().then((res) => {
-			if (!res.success) {
-				return;
-			}
+	const markAsReadMutation = useMutation({
+		mutationFn: markNotiAsRead,
+		onSuccess: (data) => {
+			if (data.success) {
+				// queryClient.setQueryData(QUERY_KEYS.USER_NOTIFICATIONS_COUNT, 0);
 
-			queryClient.invalidateQueries({
-				queryKey: QUERY_KEYS.USER_NOTIFICATIONS_COUNT,
-			});
-		});
-	}, [queryClient.invalidateQueries]);
+				queryClient.invalidateQueries({
+					queryKey: QUERY_KEYS.USER_NOTIFICATIONS_COUNT,
+				});
+			}
+		},
+	});
+
+	// biome-ignore lint: only want this to run after rendering
+	useEffect(() => {
+		markAsReadMutation.mutate();
+	}, []);
 
 	return null;
 }
