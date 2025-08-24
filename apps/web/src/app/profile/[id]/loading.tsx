06008d95f0f1c3342bcd5b0b252ab333901d332d
Facundo Personal <facundopellicer4@gmail.com>
2025-08-24 19:53:47 -0300

    remove unnecesary loading.tsx

--- apps/web/src/app/profile/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2Icon } from "lucide-react";
-
-export default function Loading() {
-	return (
-		<div className="h-svh items-center flex justify-center">
-			<Loader2Icon className="animate-spin" />
-		</div>
-	);
-}
